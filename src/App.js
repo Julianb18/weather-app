cd63d25114b012961438340bb9776a810b59a913
Julianb18 <julian141718@gmail.com>
2020-12-03 16:34:37 +0100

    mobile styling basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
 import "./App.css";
 
+const api = {
+  key: "0fe7922cb44365f6a80bd52954b3cedd",
+  base: "htpps://api.openweathermap.org/data/2.5/",
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
